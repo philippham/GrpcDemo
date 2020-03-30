3d05e11368524cab1d46a0201a2c6f84b32fa9d6
pnpham <philip.pham@renaissance.com>
2020-03-30 12:21:47 -0400

    Add codes

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (var channel = GrpcChannel.ForAddress("https://localhost:5001"))
+            {
+                var client = new GrpcServiceDemo.Greeter.GreeterClient(channel);
+
+                var request = new GrpcServiceDemo.HelloRequest()
+                {
+                    Name = "Mr Grpc"
+                };
+
+                var response = await client.SayHelloAsync(request);
+                Console.WriteLine(response.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
